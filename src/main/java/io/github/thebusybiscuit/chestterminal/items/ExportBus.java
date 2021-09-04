f39939584a1ccecff298e3f6b6feec0743769dad
md5sha256 <42793301+md5sha256@users.noreply.github.com>
2021-09-05 01:03:24 +0800

    Update to Slimefun RC-27

--- src/main/java/io/github/thebusybiscuit/chestterminal/items/ExportBus.java
+++ src/main/java/io/github/thebusybiscuit/chestterminal/items/ExportBus.java
@@ -6,23 +6,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
 
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockPlaceHandler;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu.MenuClickHandler;
-import me.mrCookieSlime.Slimefun.Lists.RecipeType;
-import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
-import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
 import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
-import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
 public class ExportBus extends SlimefunItem {
 
     private static final int[] border = { 0, 1, 3, 4, 5, 6, 9, 10, 11, 12, 13, 14, 15, 18, 22, 24, 27, 31, 33, 34, 35, 36, 40, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 32, 23, 41 };
 
-    public ExportBus(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
-        super(category, item, recipeType, recipe);
+    public ExportBus(ItemGroup itemGroup, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, item, recipeType, recipe);
 
         new BlockMenuPreset(getId(), "&3CT Export Bus") {
 
@@ -63,16 +63,16 @@ public class ExportBus extends SlimefunItem {
         MenuClickHandler click = (p, slot, item, action) -> false;
 
         for (int i : border) {
-            preset.addItem(i, new CustomItem(Material.CYAN_STAINED_GLASS_PANE, " "), click);
+            preset.addItem(i, new CustomItemStack(Material.CYAN_STAINED_GLASS_PANE, " "), click);
         }
 
-        preset.addItem(7, new CustomItem(Material.BLUE_STAINED_GLASS_PANE, " "), click);
-        preset.addItem(8, new CustomItem(Material.BLUE_STAINED_GLASS_PANE, " "), click);
-        preset.addItem(16, new CustomItem(Material.BLUE_STAINED_GLASS_PANE, " "), click);
-        preset.addItem(25, new CustomItem(Material.BLUE_STAINED_GLASS_PANE, " "), click);
-        preset.addItem(26, new CustomItem(Material.BLUE_STAINED_GLASS_PANE, " "), click);
+        preset.addItem(7, new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, " "), click);
+        preset.addItem(8, new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, " "), click);
+        preset.addItem(16, new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, " "), click);
+        preset.addItem(25, new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, " "), click);
+        preset.addItem(26, new CustomItemStack(Material.BLUE_STAINED_GLASS_PANE, " "), click);
 
-        preset.addItem(2, new CustomItem(Material.PAPER, "&3Items", "", "&bPut in all Items you want to", "&bwhitelist"), click);
+        preset.addItem(2, new CustomItemStack(Material.PAPER, "&3Items", "", "&bPut in all Items you want to", "&bwhitelist"), click);
     }
 
     public int[] getInputSlots() {
